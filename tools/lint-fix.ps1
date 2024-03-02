d79a03b43b598e3b3292debd0b425142141a390c
Kenichi Kamiya <kachick1@gmail.com>
2024-03-02 17:37:02 +0900

    Extract tool dirs
    https://github.com/PowerShell/PSScriptAnalyzer/issues/561

---
+++ tools/lint-fix.ps1
@@ -0,0 +1,7 @@
+﻿# Do NOT use 'Invoke-ScriptAnalyzer -Recurse -Path .' It includes dotfiles as .direnv
+# https://github.com/PowerShell/PSScriptAnalyzer/issues/561
+Get-ChildItem -Recurse -Path . -Include "*.ps1" |
+    ForEach-Object {
+        echo $_.FullName
+        Invoke-ScriptAnalyzer -Recurse -ReportSummary -EnableExit -Settings CodeFormatting -Fix -Path $_.FullName
+    }
