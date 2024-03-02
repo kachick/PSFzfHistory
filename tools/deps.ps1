d4c1b91fabd85a97bd44322b2d2dcd3bfedc2d95
Kenichi Kamiya <kachick1@gmail.com>
2024-03-02 17:43:30 +0900

    Extract devdependency printer

---
+++ tools/deps.ps1
@@ -0,0 +1,9 @@
+﻿# nix --version - Excluded for Windows, nix targets onle Unix-like OS
+nil --version
+task --version
+dprint --version
+typos --version
+pwsh --version
+pwsh --Command 'Get-InstalledModule'
+grep --version
+wc --version
