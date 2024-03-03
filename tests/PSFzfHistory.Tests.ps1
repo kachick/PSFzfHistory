a144c2fdbfc540a108651ed6e4ba53f67adc4c3c
Kenichi Kamiya <kachick1@gmail.com>
2024-03-03 19:09:01 +0900

    Start to use Pester for unit tests

---
+++ tests/PSFzfHistory.Tests.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module -Name .\src\PSFzfHistory.psm1
+
+InModuleScope PSFzfHistory {
+    Describe 'Reverse' {
+        It 'reverses given list' {
+          (1, 2, 3, 42, 987654321, 42) | Reverse | Should -BeExactly (42, 987654321, 42, 3, 2, 1)
+        }
+    }
+}
--- tools/test-all.ps1
+++ tools/test-all.ps1
@@ -1,1 +1,5 @@
-Import-Module -Name .\src\PSFzfHistory.psm1
+# https://pester.dev/docs/commands/New-PesterConfiguration
+$config = New-PesterConfiguration
+$config.Run.Exit = $true
+
+Invoke-Pester -Configuration $config
