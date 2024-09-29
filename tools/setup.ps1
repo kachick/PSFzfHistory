1461e02bf421444736574e0a7a4b72d50bda657c
Kenichi Kamiya <kachick1@gmail.com>
2024-09-30 00:04:00 +0900

    Specify  -ErrorAction SilentlyContinue for Get-InstalledModule

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -1,17 +1,17 @@
 ﻿$PSNativeCommandUseErrorActionPreference = $true
 $ErrorActionPreference = 'Stop'
 
-if (-not (Get-InstalledModule PSScriptAnalyzer)) {
+if (-not (Get-InstalledModule -ErrorAction SilentlyContinue PSScriptAnalyzer )) {
     # https://github.com/PowerShell/PSScriptAnalyzer
     Install-Module -Name PSScriptAnalyzer -Force
 }
 
-if (-not (Get-InstalledModule Pester)) {
+if (-not (Get-InstalledModule -ErrorAction SilentlyContinue Pester)) {
     # https://github.com/pester/pester
     Install-Module -Name Pester -Force
 }
 
-if (-not (Get-InstalledModule Benchpress)) {
+if (-not (Get-InstalledModule -ErrorAction SilentlyContinue Benchpress)) {
     # https://github.com/StartAutomating/Benchpress
     Install-Module Benchpress -Scope CurrentUser -Force
 }
