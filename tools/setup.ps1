98a56ee85ed8b08ba926e6d5411b4662c4d9de24
Kenichi Kamiya <kachick1@gmail.com>
2024-09-29 21:53:54 +0900

    Skip install module if already installed to avoid blocked process in `task default`

--- tools/setup.ps1
+++ tools/setup.ps1
@@ -1,6 +1,17 @@
-﻿# https://github.com/PowerShell/PSScriptAnalyzer
-Install-Module -Name PSScriptAnalyzer -Force
-# https://github.com/pester/pester
-Install-Module -Name Pester -Force
-# https://github.com/StartAutomating/Benchpress
-Install-Module Benchpress -Scope CurrentUser -Force
+﻿$PSNativeCommandUseErrorActionPreference = $true
+$ErrorActionPreference = 'Stop'
+
+if (-not (Get-InstalledModule PSScriptAnalyzer)) {
+    # https://github.com/PowerShell/PSScriptAnalyzer
+    Install-Module -Name PSScriptAnalyzer -Force
+}
+
+if (-not (Get-InstalledModule Pester)) {
+    # https://github.com/pester/pester
+    Install-Module -Name Pester -Force
+}
+
+if (-not (Get-InstalledModule Benchpress)) {
+    # https://github.com/StartAutomating/Benchpress
+    Install-Module Benchpress -Scope CurrentUser -Force
+}
