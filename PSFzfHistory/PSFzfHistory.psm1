1252de52eaa952c1206922312d32f6614ad41ada
Kenichi Kamiya <kachick1@gmail.com>
2026-03-02 02:16:04 +0900

    perf: achieve ultimate speed by integrating history processing into a single loop (~3x faster than Step 1)

--- PSFzfHistory/PSFzfHistory.psm1
+++ PSFzfHistory/PSFzfHistory.psm1
@@ -2,11 +2,19 @@
 
 # Do not add --height option for fzf, it shows nothing in keybind use
 function Invoke-FzfHistory ([String]$fuzzy) {
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetHistoryItems() |
-        Select-Object -ExpandProperty CommandLine |
-        Reverse |
-        AsOrderedSet |
-        fzf --no-sort --no-height --scheme=history --query=$fuzzy
+    $history = [Microsoft.PowerShell.PSConsoleReadLine]::GetHistoryItems()
+    $set = [System.Collections.Generic.HashSet[string]]::new()
+    
+    # Process history from latest to oldest with de-duplication in a single loop
+    # to avoid pipeline overhead of separate 'Reverse' and 'AsOrderedSet' functions.
+    $uniqueCommands = for ($i = $history.Count - 1; $i -ge 0; $i--) {
+        $cmd = $history[$i].CommandLine
+        if ($set.Add($cmd)) {
+            $cmd
+        }
+    }
+
+    $uniqueCommands | fzf --no-sort --no-height --scheme=history --query=$fuzzy
 }
 
 # Avoid System.Collections.Generic.SortedSet from following points
